4b2e6a2b5d7d73185d6b4f42f1fdb4ea795a8785
Fardhan Ardhi <fa.dsg.id@gmail.com>
2020-10-06 16:56:17 +0700

    setup storyboard

---
+++ lib/stories/tesStory.dart
@@ -0,0 +1,10 @@
+import 'package:bubble/widgets/tes.dart';
+import 'package:flutter/widgets.dart';
+import 'package:storyboard/storyboard.dart';
+
+class TestStory extends Story {
+  @override
+  List<Widget> get storyContent {
+    return [Tes()];
+  }
+}
---
+++ lib/widgets/tes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Tes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(onPressed: () {});
+  }
+}
